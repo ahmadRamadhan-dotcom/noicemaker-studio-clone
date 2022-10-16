22ab5713ac171787e33aff1efc084857a474e894
ahmadRamadhanJuara <ahmadoneperson@gmail.com>
2022-10-16 08:05:28 +0700

    chore: responsive header for small devices, medium devices, large devices

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="w-full border-8">
-    <h1>ha</h1>
+  <div class="w-full sm:flex sm:justify-center bg-[#101010]">
+    <div
+      class="sm:w-[93%] md:w-4/5 lg:w-10/12 xl:w-[88%] sm:flex sm:justify-between sm:py-3 xl:py-3"
+    >
+      <figure>
+        <img
+          src="/noicemaker.svg"
+          alt="noice maker header logo"
+          class="sm:h-8 xl:h-8"
+        />
+      </figure>
+      <a
+        href="#_"
+        class="inline-flex tracking-[0.2em] items-center justify-center sm:text-[0.75rem] sm:py-1 sm:px-4 xl:px-4 xl:py-1 xl:text-[0.75rem] uppercase xl:font-bold font-medium text-white whitespace-no-wrap rounded-md bg-black border border-gray-200 shadow-sm"
+      >
+        Masuk
+      </a>
+    </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,5 @@
 <template>
-  <div id="home">
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad provident
-      eum, reiciendis ullam quod facilis. Libero fugit architecto doloremque
-      voluptates, fugiat cum velit laudantium officia animi iure cupiditate
-      perferendis corrupti suscipit illum consequatur repellat omnis. Ducimus
-      veritatis laboriosam officia aspernatur, optio sit unde qui vero vitae
-      quidem voluptatem eum quod accusantium dolorum eveniet cumque sed id ex
-      alias iusto? Tempora ipsa rerum earum at totam similique quaerat sint,
-      molestias rem nesciunt hic nisi. Minima ea voluptates iure dolorum impedit
-      tempora eum nemo quisquam illum, nihil ipsa laborum beatae quidem
-      exercitationem sint eveniet, voluptas soluta voluptatibus optio
-      accusantium perspiciatis repellendus similique.
-    </p>
-  </div>
+  <div id="home"></div>
 </template>
 
 <script setup>
